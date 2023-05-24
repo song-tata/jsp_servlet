6e177402dae624ea305a49eb67037d9e812bd7eb
song_tata <kgsong8020@naver.com>
2023-05-24 10:32:51 +0900

    feat : chap 10 시작

---
+++ chap10-2/src/main/java/sec01/ex01/loginImpl.java
@@ -0,0 +1,32 @@
+package sec01.ex01;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class loginImpl
+ *
+ */
+@WebListener
+public class loginImpl implements HttpSessionListener {
+	String u_id;
+	String u_pw;
+	static int total_user=0;
+	
+    public loginImpl() {
+    }
+    public loginImpl(String u_id,String u_pw) {
+    	this.u_id = u_id;
+    	this.u_pw = u_pw;
+    }
+
+    public void sessionCreated(HttpSessionEvent se)  { 
+    	System.out.println("세션 생성");
+    	++
+    }
+
+    public void sessionDestroyed(HttpSessionEvent se)  { 
+    }
+	
+}
